bb9b95afd604bba1600166ce7f4ecee7e8a17a96
Bryan <bryanfinesw@gmail.com>
2025-03-11 00:49:08 -0400

    + Updated views and added Item Form view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,16 @@
-const categoryController = require("./categoryController.js");
-const itemController = require("./itemController.js");
+const db = require("../db/queries.js");
 
 function homePage(req, res) {
-    res.render("index");
+    res.render("index", { title: "Inventory App"});
+}
+
+async function getAddItemForm(req, res) {
+    const categories = await db.getAllCategories();
+
+    res.render("addItem", { title: "Add Item", categories: categories });
 }
 
 module.exports = {
     homePage,
+    getAddItemForm
 }
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,5 +3,6 @@ const indexController = require("../controllers/indexController.js");
 const router = Router();
 
 router.get("/", indexController.homePage);
+router.get("/add-item", indexController.getAddItemForm);
 
 module.exports = router;
